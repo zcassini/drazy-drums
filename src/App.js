cfddd097af8705eaa60980b507fe725ef155f733
Zac Cassini <zcassini@gmail.com>
2017-01-01 14:04:59 -0500

    add gh corner and fix code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import GithubCorner from 'react-github-corner'
 
 import clap    from './sounds/clap.wav'
 import hihat   from './sounds/hihat.wav'
@@ -11,19 +12,17 @@ import snare   from './sounds/snare.wav'
 import tom     from './sounds/tom.wav'
 import tink    from './sounds/tink.wav'
 
-
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return; // skip it if its not a transition
-  this.classList.remove('playing');
-  console.log(e.propertyName);
+function removeTransition (e) {
+  if (e.propertyName !== 'transform') return // skip it if its not a transition
+  this.classList.remove('playing')
 }
 
-const Key = (props) => {
+const Key = props => {
   return (
-    <div className='key' key={props.dataKey} onClick={() => props.handleClick(props.name)} accessKey={props.letter}>
+    <div className='key' data-key={props['data-key']} key={props['data-key']} onClick={() => props.handleClick(props.name)}>
       <p>{props.letter}</p>
-      <span className="sound">{props.snd}</span>
-      <audio id={props.name} src={props.snd} preload="auto"></audio>
+      <span className="sound">{props.name}</span>
+      <audio id={props.name} src={props.snd} data-key={props['data-key']} preload="auto"></audio>
     </div>
   )
 }
@@ -32,56 +31,54 @@ class App extends Component {
   constructor () {
     super()
     this.state = { keys: [
-      {dataKey: 65, name: 'clap',    snd: clap},
-      {dataKey: 83, name: 'hihat',   snd: hihat},
-      {dataKey: 68, name: 'kick',    snd: kick},
-      {dataKey: 70, name: 'openhat', snd: openhat},
-      {dataKey: 71, name: 'boom',    snd: boom},
-      {dataKey: 72, name: 'ride',    snd: ride},
-      {dataKey: 74, name: 'snare',   snd: snare},
-      {dataKey: 75, name: 'tom',     snd: tom},
-      {dataKey: 76, name: 'tink',    snd: tink}
+      {'data-key': 65, name: 'clap',    snd: clap},
+      {'data-key': 83, name: 'hihat',   snd: hihat},
+      {'data-key': 68, name: 'kick',    snd: kick},
+      {'data-key': 70, name: 'openhat', snd: openhat},
+      {'data-key': 71, name: 'boom',    snd: boom},
+      {'data-key': 72, name: 'ride',    snd: ride},
+      {'data-key': 74, name: 'snare',   snd: snare},
+      {'data-key': 75, name: 'tom',     snd: tom},
+      {'data-key': 76, name: 'tink',    snd: tink}
     ]}
   }
   handleClick (id) {
     console.log(id)
     const ele = document.getElementById(id)
+    ele.currentTime = 0
     ele.play()
     ele.parentNode.classList.add('playing')
   }
   componentDidMount () {
-    const divs = document.querySelectorAll('.key') //.forEach(k => k.addEventListener('transitionend', removeTransition))
-    for (let div of divs) {
-      div.addEventListener('transitionend', removeTransition)
-    }
+    document.querySelectorAll('.key').forEach(k => k.addEventListener('transitionend', removeTransition))
+    window.addEventListener('keydown', playSound)
   }
   render () {
     const keys = this.state.keys.map(e =>
-      <Key dataKey={e.dataKey}
-           letter={String.fromCharCode(e.dataKey)}
-           key={e.dataKey}
+      <Key key={e['data-key']}
+           data-key={e['data-key']}
+           letter={String.fromCharCode(e['data-key'])}
            name={e.name}
            snd={e.snd}
            handleClick={this.handleClick}/>)
     return (
-      <div className="keys">
-       {keys}
+      <div>
+        <GithubCorner href="https://github.com/zcassini/drazy-drums" />
+        <div className="keys">
+          {keys}
+        </div>
       </div>
     )
   }
 }
 
-
-/* 
- * function playSound (e) {
- *   var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]')
- *   var key = document.querySelector('.key[data-key="' + e.keyCode + '"]')
- *   if (!audio) return // stops the function if no key associated with audio
- *   audio.currentTime = 0 // rewinds to the start
- *   audio.play()
- *   key.classList.add('playing')
- * }
- * 
- * window.addEventListener('keydown', playSound);*/
+function playSound (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector('.key[data-key="' + e.keyCode + '"]')
+  if (!audio) return // stops the function if no key associated with audio
+  audio.currentTime = 0 // rewinds to the start
+  audio.play()
+  key.classList.add('playing')
+}
 
 export default App
